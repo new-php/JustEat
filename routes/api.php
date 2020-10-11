0c9b096d85daacc33c6c9a07d676c71253df34e8
Xavier de Juan Pulido <xavidejuan@gmail.com>
2020-10-11 18:49:31 +0200

    Setup Passport Authentication and versioning api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix(config('api.version'))->group(function () {
+
+	// Write routes here to be APP_URL/api/API_VERSION/...
+
+});
