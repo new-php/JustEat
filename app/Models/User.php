bed74c231f9328019043c72ec5fd4f985cfe2da1
Xavier de Juan Pulido <xavidejuan@gmail.com>
2020-10-23 18:40:28 +0200

    Add spatie/laravel-permission + Laravel-Backpack/PermissionManager

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 
 class User extends Authenticatable
 {
-    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, CrudTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -47,10 +48,5 @@ class User extends Authenticatable
     public function addresses()
     {
         return $this->hasMany('App\Models\Address');
-    }
-
-    public function roles()
-    {
-        return $this->belongsToMany('App\Models\Role');
     }
 }
