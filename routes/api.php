774bc38c0ce265cd8e5f3b2898913ce6f23caab1
Xavier de Juan Pulido <xavidejuan@gmail.com>
2020-10-25 13:08:38 +0100

    autogenerated auth

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,18 @@ Route::prefix(config('api.version'))->group(function () {
 
 	// Write routes here to be APP_URL/api/API_VERSION/...
 
+	// Authentication routes
+	Route::get('login', 'Auth\LoginController@showLoginForm');
+	Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+	Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+	Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+	Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+    Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
+    Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+	Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+	Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
 });
