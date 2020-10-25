61df819141bf5e2160ef468b68f8633985e4618a
Xavier de Juan Pulido <xavidejuan@gmail.com>
2020-10-25 17:33:15 +0100

    Set Product  photo attribute

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -40,6 +40,14 @@ class Product extends Model
     // protected $hidden = [];
     // protected $dates = [];
 
+    public static function boot()
+    {
+        parent::boot();
+        static::deleting(function($obj) {
+            \Storage::disk('public')->delete($obj->photo);
+        });
+    }
+
     public function productCategories()
     {
         return $this->belongsToMany('App\Models\ProductCategory');
@@ -53,5 +61,46 @@ class Product extends Model
     public function orderItems()
     {
         return $this->hasMany('App\Models\OrderItem');
+    }
+
+    public function setPhotoAttribute($value)
+    {
+        $attribute_name = "photo";
+        // or use your own disk, defined in config/filesystems.php
+        $disk = 'public'; 
+        // destination path relative to the disk above
+        $destination_path = "images/products"; 
+
+        // if the image was erased
+        if ($value==null) {
+            // delete the image from disk
+            \Storage::disk($disk)->delete($this->{$attribute_name});
+
+            // set null in the database column
+            $this->attributes[$attribute_name] = null;
+        }
+
+        // if a base64 was sent, store it in the db
+        if (Str::startsWith($value, 'data:image'))
+        {
+            // 0. Make the image
+            $image = \Image::make($value)->encode('jpg', 90);
+
+            // 1. Generate a filename.
+            $filename = md5($value.time()).'.jpg';
+
+            // 2. Store the image on disk.
+            \Storage::disk($disk)->put($destination_path.'/'.$filename, $image->stream());
+
+            // 3. Delete the previous image, if there was one.
+            \Storage::disk($disk)->delete($this->{$attribute_name});
+
+            // 4. Save the public path to the database
+            // but first, remove "public/" from the path, since we're pointing to it 
+            // from the root folder; that way, what gets saved in the db
+            // is the public URL (everything that comes after the domain name)
+            $public_destination_path = Str::replaceFirst('public/', '', $destination_path);
+            $this->attributes[$attribute_name] = $public_destination_path.'/'.$filename;
+        }
     }
 }
