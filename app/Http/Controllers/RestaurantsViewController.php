06bebafc42e6ed8a9a04cb2ff69386dbe0367087
Xavier de Juan Pulido <xavidejuan@gmail.com>
2020-11-10 23:39:05 +0100

    Get /user endpoint

--- app/Http/Controllers/RestaurantsViewController.php
+++ app/Http/Controllers/RestaurantsViewController.php
@@ -9,12 +9,12 @@ class RestaurantsViewController extends Controller
 {
     public function restaurantsPage()
     {
-        return view('restaurants.restaurants-page', ['user' => auth('api')->user(), 'address' => 'Carrer Congrés, 08031 Barcelona']);
+        return view('restaurants.restaurants-page', ['address' => 'Carrer Congrés, 08031 Barcelona']);
     }
 
     public function restaurantPage(Restaurant $restaurant)
     {
-        return view('restaurants.restaurant-page', ['user' => auth('api')->user(), 'restaurant' => $restaurant]);
+        return view('restaurants.restaurant-page', ['restaurant' => $restaurant]);
     }
 }
 
