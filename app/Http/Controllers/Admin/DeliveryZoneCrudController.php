e56876f04e2298c756dbb287aabd19340efa69ba
Xavier de Juan Pulido <xavidejuan@gmail.com>
2020-11-12 19:34:02 +0100

    Fix postal codes, integer to string

--- app/Http/Controllers/Admin/DeliveryZoneCrudController.php
+++ app/Http/Controllers/Admin/DeliveryZoneCrudController.php
@@ -22,7 +22,7 @@ class DeliveryZoneCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -34,7 +34,7 @@ class DeliveryZoneCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
@@ -42,7 +42,7 @@ class DeliveryZoneCrudController extends CrudController
     {
         CRUD::addColumn([
             'label' => 'Restaurant',
-            'type' => 'select', 
+            'type' => 'select',
             'name' => 'restaurant_id',
             'entity' => 'restaurant',
             'attribute' => 'name',
@@ -50,29 +50,29 @@ class DeliveryZoneCrudController extends CrudController
             ]);
         CRUD::addColumn([
             'label' => 'ZIP Code',
-            'type' => 'number', 
+            'type' => 'text',
             'name' => 'postal_code',
             ]);
         CRUD::addColumn([
             'label' => 'Min order price',
-            'type' => 'number', 
+            'type' => 'number',
             'name' => 'min_order_price',
             ]);
         CRUD::addColumn([
             'label' => 'Delivery price',
-            'type' => 'number', 
+            'type' => 'number',
             'name' => 'delivery_price',
             ]);
         CRUD::addColumn([
             'label' => 'Delivery time',
-            'type' => 'number', 
+            'type' => 'number',
             'name' => 'delivery_time',
             ]);
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -82,7 +82,7 @@ class DeliveryZoneCrudController extends CrudController
 
         CRUD::addField([
             'label' => 'Restaurant',
-            'type' => 'select2', 
+            'type' => 'select2',
             'name' => 'restaurant_id',
             'entity' => 'restaurant',
             'attribute' => 'name',
@@ -91,28 +91,29 @@ class DeliveryZoneCrudController extends CrudController
         CRUD::addField([
             'name' => 'postal_code',
             'label' => 'ZIP Code',
-            'type' => 'number', 
+            'type' => 'text',
             ]);
         CRUD::addField([
             'name' => 'min_order_price',
             'label' => 'Min order price',
-            'type' => 'number', 
+            'type' => 'number',
             ]);
         CRUD::addField([
             'name' => 'delivery_price',
             'label' => 'Delivery price',
-            'type' => 'number', 
+            'type' => 'number',
+            'attributes' => ["step" => "any"],
             ]);
         CRUD::addField([
             'name' => 'delivery_time',
             'label' => 'Delivery time',
-            'type' => 'number', 
+            'type' => 'number',
             ]);
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
