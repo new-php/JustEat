5daced6fcbd9ba65a715b2223a6bf0a0b2f840cf
Xavier de Juan Pulido <xavidejuan@gmail.com>
2020-11-13 18:06:25 +0100

    fix restaurant view parameters

--- app/Http/Controllers/RestaurantsViewController.php
+++ app/Http/Controllers/RestaurantsViewController.php
@@ -10,7 +10,6 @@ class RestaurantsViewController extends Controller
 {
     public function restaurantsPage()
     {
-        $zip = $request->input('zip')
         $restaurants = Restaurant::select('id','name','photo','logo')->with('categories', 'ratings', 'deliveryZones')->get();
 
         foreach($restaurants as $restaurant) {
@@ -18,10 +17,9 @@ class RestaurantsViewController extends Controller
             $restaurant->number_of_ratings = $restaurant->ratings->count();
             $restaurant->price_delivery = $restaurant->deliveryZones->avg('delivery_price');
             $restaurant->min_order_price = $restaurant->deliveryZones->avg('min_order_price');
-            $restaurant->min_delivery_time = $restaurant->deliveryZones->min('min_order_price');
-            $restaurant->max_delivery_time = $restaurant->deliveryZones->max('min_order_price');
+            $restaurant->min_delivery_time = $restaurant->deliveryZones->min('delivery_time');
+            $restaurant->max_delivery_time = $restaurant->deliveryZones->max('delivery_time');
         }
-        //return $restaurants;
         return view('restaurants.restaurants-page', ['address' => 'Carrer Congrés, 08031 Barcelona', 'restaurants' => $restaurants]);
     }
 
