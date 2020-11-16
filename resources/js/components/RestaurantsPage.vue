b6946553890084024cbc2993c48a3f3493d04794
gerardPuch <pukipuch@gmail.com>
2020-11-16 22:22:07 +0000

    B02Task5

--- resources/js/components/RestaurantsPage.vue
+++ resources/js/components/RestaurantsPage.vue
@@ -276,15 +276,9 @@
                 photo: "storage/images/restaurants/060a38a121bb843bd30b806d677b6044.jpg",
                 logo: "storage/images/restaurants/d395b692430196de511d4603c43ea5da.jpg",
                 categories: [
-                    {
-                        id: 1,
-                        name: "Americana"
-                    },
-                    {
-                        id: 2,
-                        name: "Argentina"
-                    },
-                ],
+                    this.categories[0],
+                    this.categories[1]
+                    ],
                 average_rating: 4.7,
                 number_of_ratings: 123,
                 price_delivery: 2,
@@ -298,10 +292,7 @@
                 photo: "storage/images/restaurants/060a38a121bb843bd30b806d677b6044.jpg",
                 logo: "storage/images/restaurants/d395b692430196de511d4603c43ea5da.jpg",
                 categories: [
-                    {
-                        id: 1,
-                        name: "Americana"
-                    },
+                    this.categories[0]
                 ],
                 average_rating: 3.7,
                 number_of_ratings: 123,
@@ -316,18 +307,8 @@
                 photo: "storage/images/restaurants/060a38a121bb843bd30b806d677b6044.jpg",
                 logo: "storage/images/restaurants/d395b692430196de511d4603c43ea5da.jpg",
                 categories: [
-                    {
-                        id: 1,
-                        name: "Americana"
-                    },
-                    {
-                        id: 2,
-                        name: "Argentina"
-                    },
-                    {
-                        id: 3,
-                        name: "Bao",
-                    }
+                    this.categories[0],
+                    this.categories[3]
                 ],
                 average_rating: 3.2,
                 number_of_ratings: 123,
@@ -342,10 +323,7 @@
                 photo: "storage/images/restaurants/060a38a121bb843bd30b806d677b6044.jpg",
                 logo: "storage/images/restaurants/d395b692430196de511d4603c43ea5da.jpg",
                 categories: [
-                    {
-                        id: 1,
-                        name: "Americana"
-                    },
+                    this.categories[3]
                 ],
                 average_rating: 1.5,
                 number_of_ratings: 123,
@@ -360,10 +338,7 @@
                 photo: "storage/images/restaurants/060a38a121bb843bd30b806d677b6044.jpg",
                 logo: "storage/images/restaurants/d395b692430196de511d4603c43ea5da.jpg",
                 categories: [
-                    {
-                        id: 1,
-                        name: "Americana"
-                    },
+                    this.categories[0]
                 ],
                 average_rating: 2,
                 number_of_ratings: 123,
@@ -387,7 +362,6 @@
         }
     },
     mounted() {
-
     },
     watch: {
         search_input: function(val, oldVal) {
@@ -426,6 +400,7 @@
             if (!this.categories_selected.includes(category)) {
                 this.categories_selected.push(category);
                 $("#mainCategory-" + category.id).addClass("categories-active");
+                this.showForCategoryRestaurants(category);
             } else {
                 for (let i = this.categories_selected.length-1; i >= 0; i-- ) {
                     if (this.categories_selected[i].id == category.id) {
@@ -433,6 +408,7 @@
                     }
                 }
                 $("#mainCategory-" + category.id).removeClass("categories-active");
+                this.hideForCategoryRestaurants(category);
             }
         },
         selectFilter(filter) {
@@ -446,6 +422,46 @@
                     }
                 }
                 $("#" + filter).removeClass("filter-active");
+            }
+        },
+        showForCategoryRestaurants(category){
+            if(this.categories_selected.length <= 1){
+                this.restaurants_showing = [];
+            }
+
+            if(this.categories_selected.includes(category)){
+                for(let i = this.restaurants.length-1; i >= 0; i--){
+                    if(this.restaurants[i].categories.includes(category)){
+                        if(!this.restaurants_showing.includes(this.restaurants[i])){
+                            this.restaurants_showing.push(this.restaurants[i]);
+                        }
+                    }
+                }
+            }
+        },
+        hideForCategoryRestaurants(category){
+            if(this.categories_selected.length == 0){
+                this.restaurants_showing = this.restaurants;
+            }
+            else{ 
+                for(let i = this.restaurants_showing.length-1; i >= 0; i--){
+                    if(this.restaurants_showing[i].categories.includes(category)){
+                        var otherCategory = false;
+                        for(let j = this.restaurants_showing[i].categories.length-1; j >= 0; j--){
+                            if(this.categories_selected.includes(this.restaurants_showing[i].categories[j])){
+                                otherCategory = true;
+                            }
+                        }
+
+                        if(!otherCategory){
+                            for (let j = this.restaurants_showing.length-1; j >= 0; j-- ) {
+                                if (this.restaurants_showing[j] == this.restaurants_showing[i]) {
+                                    this.restaurants_showing.splice(j,1);
+                                }
+                            }
+                        }
+                    }
+                }
             }
         },
         goToRestaurant(restaurant) {
