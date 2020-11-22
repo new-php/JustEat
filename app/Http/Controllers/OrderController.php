737d7eca76e2cbf5274230e84307b6f64b1f48b0
itslorenzomg <lorenzovigodelrosso@gmail.com>
2020-11-22 12:35:37 +0100

    Fixed user assignment typo

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -46,7 +46,7 @@ class OrderController extends Controller
         };
 
         $order = Order::create([
-            'user_id' => user,
+            'user_id' => $user,
             'restaurant_id' => $request->restaurant_id,
             'rider_id' => 0,         # TODO change rider_id assignment
             'details' => $request->details,
