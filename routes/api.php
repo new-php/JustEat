a7f574250341ac8b97b3236ed184f4e3f4a679a5
itslorenzomg <lorenzovigodelrosso@gmail.com>
2020-11-29 09:23:31 +0100

    Fix URLs 3.0

--- routes/api.php
+++ routes/api.php
@@ -36,8 +36,8 @@ Route::prefix(config('api.version'))->group(function () {
 
     Route::middleware('auth:api')->group(function() {
         Route::post('order', 'OrderController@store')->name('order.new');        
-        Route::put('order/address', 'OrderController@addAddress')->name('order.address');
-        Route::put('order/pay', 'OrderController@pay')->name('order.pay');
+        Route::put('order/{id}/address', 'OrderController@addAddress')->name('order.address');
+        Route::put('order/{id}/pay', 'OrderController@pay')->name('order.pay');
         Route::get('user', 'UserController@show')->name('user');
     });
 });
