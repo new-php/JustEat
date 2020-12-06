62dae196393b97df82a531f57e141691d348d807
Xavier de Juan Pulido <xavidejuan@gmail.com>
2020-12-06 18:17:37 +0100

    Category Model tests

---
+++ tests/Unit/CategoryModelTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Category;
+use App\Models\Restaurant;
+
+class CategoryModelTest extends TestCase
+{
+    /**
+     * Restaurants relationship
+     *
+     * @return void
+     */
+    public function testRestaurantsRelationship()
+    {
+        $category = Category::factory()->create();
+        $restaurant = Restaurant::factory()->create();
+
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $category->restaurants);
+    }
+}
