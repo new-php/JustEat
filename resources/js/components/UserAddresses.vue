a2899526de0c97b2bced8119b65bf6e8decc6054
osemenso7 <oscarsemente100@gmail.com>
2020-12-10 15:44:10 +0100

    user-addresses-vue

---
+++ resources/js/components/UserAddresses.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="addresses-container">
+        <h1 class="title">Libreta de direcciones</h1>
+
+        <p class="info-text">
+            Crea y guarda direcciones de entrega para hacer tus pedidos <br> 
+            fácilmente
+        </p>
+
+        <section class="addresses" v-for="address in list_addresses" :key="address.id" :id="'address-' + address.id">
+            <strong>{{address.postal_code}}</strong>
+            <span>
+                <a href="/account" class="link-edit" id="link-edit">
+                    <strong>Editar <br></strong>
+                </a>
+            </span>
+
+
+            <strong class="link-delete" v-on:click="deleteAddress(address)" id="link-delete">
+                Eliminar
+            </strong>
+
+
+            <div class="name-address">
+                {{address.address_line_1}}
+            </div>
+
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UserAddresses",
+        props: ['addresses'],
+        data() {
+            return {
+                list_addresses: this.addresses,
+            }
+        },
+
+        methods: {
+            deleteAddress: function(product) {
+                for (let i = 0; i < this.list_addresses.length; i++) {
+                    if (product.id == this.list_addresses[i].id) {
+                        this.list_addresses.splice(i, 1);
+                    }
+                }
+
+                if (this.list_addresses.length <= 0) {
+                    this.list_addresses = null;
+                }
+            },
+        },
+
+    };
+</script>
--- tests/Browser/UserAccountPageTest.php
+++ tests/Browser/UserAccountPageTest.php
@@ -7,6 +7,7 @@ use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Tests\Browser\Pages\UserAccountPage;
 use App\Models\User;
+use App\Models\Address;
 
 class UserAccountPageTest extends DuskTestCase
 {
@@ -45,6 +46,92 @@ class UserAccountPageTest extends DuskTestCase
                 ->assertSee('Direcciones de reparto')
                 ->click('#contacto')
                 ->assertSee('Preferencias de contacto');
+        });
+    }
+
+    /**
+     * A Dusk test example.
+     * @group accListAddresses
+     * @return void
+     */
+    public function testAddressText()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new UserAccountPage)   
+                ->click('#dir-reparto')            
+                ->assertSee('Libreta de direcciones')
+                ->assertSee('Crea y guarda direcciones de entrega para hacer tus pedidos')
+                ->assertSee('fácilmente');
+        });
+    }
+
+    /**
+     * A Dusk test example.
+     * @group accListAddresses
+     * @return void
+     */
+    public function testSeeAddress()
+    {
+
+        $address = Address::factory()->create([
+            'postal_code' => "08950",
+            'address_line_1' => "Carrer dels Caquis, 2, Bajos, 2, 08950, Espluguas de Llobregat",
+        ]);
+
+        $this->browse(function (Browser $browser) use ($address) {
+            $browser->visit(new UserAccountPage)   
+                ->click('#dir-reparto')            
+                ->assertSee($address->postal_code)
+                ->assertSee($address->address_line_1);
+        });
+    }
+
+    /**
+     * A Dusk test example.
+     * @group accListAddresses
+     * @return void
+     */
+    public function testLinkEdit()
+    {
+
+        $address = Address::factory()->create([
+            'postal_code' => "08950",
+            'address_line_1' => "Carrer dels Caquis, 2, Bajos, 2, 08950, Espluguas de Llobregat",
+        ]);
+
+        $this->browse(function (Browser $browser) use ($address) {
+            $browser->visit(new UserAccountPage)   
+                ->click('#dir-reparto')            
+                ->assertSee($address->postal_code)
+                ->assertSee($address->address_line_1)
+                ->seeLink('#link-edit')
+                ->clickLink('Editar')
+                ->assertPathIs("/");
+        });
+    }
+
+    /**
+     * A Dusk test example.
+     * @group accListAddresses
+     * @return void
+     */
+    public function testLinkDelete()
+    {
+
+        $address = Address::factory()->create([
+            'postal_code' => "08950",
+            'address_line_1' => "Carrer dels Caquis, 2, Bajos, 2, 08950, Espluguas de Llobregat",
+        ]);
+
+        $this->browse(function (Browser $browser) use ($address) {
+            $browser->visit(new UserAccountPage)   
+                ->click('#dir-reparto')            
+                ->assertSee($address->postal_code)
+                ->assertSee($address->address_line_1)
+                ->seeLink('#link-delete')
+                ->clickLink('Eliminar')
+                ->assertDontSee($address->postal_code)
+                ->assertDontSee($address->address_line_1);
         });
     }
 }
