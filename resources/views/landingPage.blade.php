abd07e24257b092410030eaaebfebfe75a805f76
MSBeatles <kamehamemarc@gmail.com>
2020-12-12 20:33:05 +0100

    Landing Page excepte enllaç

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,444 @@
+@extends('layouts.app')
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>JUBstEat | Landing Page</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="/public/landing/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="/public/landing/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="/public/landing/css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="/public/landing/css/magnific-popup.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="/public/landing/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="/public/landing/css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="/public/landing/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="/public/landing/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2">
+							<div id="colorlib-logo"><a href="#">JUBstEat</a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+
+		<section id="home" class="video-hero2" style="height: 700px; background-image: url(public/landing/img/cover_img_1.jpg);  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
+		<div class="overlay"></div>
+			<a class="player" data-property="{videoURL:'https://www.youtube.com/watch?v=ZgNu77oiKo8&ab_channel=4ksamples',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:true, startAt:0, opacity:1, quality:'default'}"></a>
+			<!--https://www.youtube.com/watch?v=vqqt5p0q-eU  gent caminant-->
+			<!--https://www.youtube.com/watch?v=WdFVMQQVUJk  hipster shit-->
+			<!--https://www.youtube.com/watch?v=5PV8y5UNAik  electrocardiograma-->
+			<div class="display-t text-center">
+				<div class="display-tc">
+					<div class="container">
+						<div class="col-md-12 col-md-offset-0">
+							<div class="animate-box">
+								<h2>Your local restaurants</h2>
+								<p>without leaving the comfort of your home</p>
+								<p><a href="/register" class="btn btn-primary btn-lg btn-custom">Sign me up!</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="colorlib-featured">
+			<div class="row animate-box">
+				<div class="featured-wrap">
+					<div class="owl-carousel">
+						<div class="item">
+							<div class="col-md-8 col-md-offset-2">
+								<div class="featured-entry">
+									<img class="img-responsive" src="public/landing/img/baconcheese.jpg" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="col-md-8 col-md-offset-2">
+								<div class="featured-entry">
+									<img class="img-responsive" src="public/landing/img/sushi.png" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="col-md-8 col-md-offset-2">
+								<div class="featured-entry">
+									<img class="img-responsive" src="public/landing/img/goulash.jpg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="colorlib-services colorlib-bg-white">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="icon-download"></i>
+							</span>
+							<div class="desc">
+								<h3>Download our app</h3>
+								<p>Available on both the Google Play Store and the App Store.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="icon-layers"></i>
+							</span>
+							<div class="desc">
+								<h3>Tons to choose</h3>
+								<p>You can choose from our selection of over 696969 restaurants.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="icon-browser"></i>
+							</span>
+							<div class="desc">
+								<h3>Don't even move!</h3>
+								<p>Once your order is done, you won't even have to leave your home! We'll get it to you.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="colorlib-intro">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center colorlib-heading animate-box">
+						<h2>We support local businesses</h2>
+						<p>Living in these hard times, our app allows small and local businesses to keep working, even through the pandemic! </p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 animate-box">
+						<span class="play"><a href="#" class="pulse popup-vimeo"><i class="icon-play3"></i></a></span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="colorlib-work-featured colorlib-bg-white">
+			<div class="container">
+				<div class="row mobile-wrap">
+					<div class="col-md-5 animate-box">
+						<div class="mobile-img" style="background-image: url(/public/landing/img/mobile-2.png);"></div>
+					</div>
+					<div class="col-md-7 animate-box">
+						<div class="desc">
+							<h2>Great customer service and communication!</h2>
+							<div class="features">
+								<span class="icon"><i class="icon-lightbulb"></i></span>
+								<div class="f-desc">
+									<p>Because we know that waiting for food without knowing when it will come is a pain, we put the best tools at your disposal, so you can contact your rider anytime!  </p>
+								</div>
+							</div>
+							<div class="features">
+								<span class="icon"><i class="icon-circle-compass"></i></span>
+								<div class="f-desc">
+									<p>Check the map to see where your order is, and track it so you can wait with a watering mouth!</p>
+								</div>
+							</div>
+							<div class="features">
+								<span class="icon"><i class="icon-beaker"></i></span>
+								<div class="f-desc">
+									<p>You can even see the expected time of delivery!</p>
+								</div>
+							</div>
+							<p><a href="/register" class="btn btn-primary btn-outline with-arrow">Please, sign me up!<i class="icon-arrow-right3"></i></a></p>
+						</div>
+					</div>
+				</div>
+				<div class="row mobile-wrap">
+					<div class="col-md-5 col-md-push-7 animate-box">
+						<div class="mobile-img" style="background-image: url(/public/landing/img/mobile-1.jpg);"></div>
+					</div>
+					<div class="col-md-7 col-md-pull-5 animate-box">
+						<div class="desc">
+							<h2>Top-notch technology</h2>
+							<div class="features">
+								<span class="icon"><i class="icon-lightbulb"></i></span>
+								<div class="f-desc">
+									<p>Created using Laravel, an open-source php-based web framework, to deliver you the best service possible.</p>
+								</div>
+							</div>
+							<div class="features">
+								<span class="icon"><i class="icon-circle-compass"></i></span>
+								<div class="f-desc">
+									<p>Tested with Dusk, a Laravel library designed specifically for the purpose.</p>
+								</div>
+							</div>
+							<div class="features">
+								<span class="icon"><i class="icon-beaker"></i></span>
+								<div class="f-desc">
+									<p>Using Vue to create a fully responsive website.</p>
+								</div>
+							</div>
+							<p><a href="/register" class="btn btn-primary btn-outline with-arrow">Start me up! <i class="icon-arrow-right3"></i></a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="colorlib-about">
+			<div class="row">
+				<div class = "col-md-12 text-center animate-box">
+					<h1>Meet our team</h1>
+				</div>
+				<div class="col-md-1 text-center">
+				</div>
+				<div class="col-md-2 text-center animate-box">
+					<div class="staff-entry">
+						<a href="#" class="staff-img" style="background-image: url(/public/landing/img/xavi.jpg);"></a>
+						<div class="desc">
+							<h3>Xavier de Juan Pulido</h3>
+							<span>Full-Stack Dev, DevOps</span>
+							<p>One of our team members</p>
+							<p>
+								<ul class="colorlib-social-icons">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-facebook"></i></a></li>
+									<li><a href="#"><i class="icon-linkedin"></i></a></li>
+									<li><a href="#"><i class="icon-dribbble"></i></a></li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 text-center animate-box">
+					<div class="staff-entry">
+						<a href="#" class="staff-img" style="background-image: url(/public/landing/img/gerard.jpg);"></a>
+						<div class="desc">
+							<h3>Gerard Puch Camacho</h3>
+							<span>Full-Stack Dev</span>
+							<p>One of our team members</p>
+							<p>
+								<ul class="colorlib-social-icons">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-facebook"></i></a></li>
+									<li><a href="#"><i class="icon-linkedin"></i></a></li>
+									<li><a href="#"><i class="icon-dribbble"></i></a></li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 text-center animate-box">
+					<div class="staff-entry">
+						<a href="#" class="staff-img" style="background-image: url(images/person3.jpg);"></a>
+						<div class="desc">
+							<h3>Óscar Sementé Solà</h3>
+							<span>Frontend Dev</span>
+							<p>One of our team members</p>
+							<p>
+								<ul class="colorlib-social-icons">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-facebook"></i></a></li>
+									<li><a href="#"><i class="icon-linkedin"></i></a></li>
+									<li><a href="#"><i class="icon-dribbble"></i></a></li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 text-center animate-box">
+					<div class="staff-entry">
+						<a href="#" class="staff-img" style="background-image: url(/public/landing/img/marc.jpg);"></a>
+						<div class="desc">
+							<h3>Marc Soler Bages</h3>
+							<span>PO, Frontend Dev</span>
+							<p>One of our team members</p>
+							<p>
+								<ul class="colorlib-social-icons">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-facebook"></i></a></li>
+									<li><a href="#"><i class="icon-linkedin"></i></a></li>
+									<li><a href="#"><i class="icon-dribbble"></i></a></li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-2 text-center animate-box">
+					<div class="staff-entry">
+						<a href="#" class="staff-img" style="background-image: url(/public/landing/img/lorenzo.jpeg);"></a>
+						<div class="desc">
+							<h3>Lorenzo Andrés Vigo del Rosso</h3>
+							<span>Bakend Dev, QA</span>
+							<p>One of our team members</p>
+							<p>
+								<ul class="colorlib-social-icons">
+									<li><a href="#"><i class="icon-twitter"></i></a></li>
+									<li><a href="#"><i class="icon-facebook"></i></a></li>
+									<li><a href="#"><i class="icon-linkedin"></i></a></li>
+									<li><a href="#"><i class="icon-dribbble"></i></a></li>
+								</ul>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="colorlib-counter" class="colorlib-counters" style="background-image: url(images/cover_img_1.jpg);" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="col-md-4 col-sm-4 text-center animate-box">
+							<div class="counter-entry">
+								<span class="icon"><i class="flaticon-ribbon"></i></span>
+								<div class="desc">
+									<span class="colorlib-counter js-counter" data-from="0" data-to="400" data-speed="5000" data-refresh-interval="50"></span>
+									<span class="colorlib-counter-label">Riders at your disposal</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4 text-center animate-box">
+							<div class="counter-entry">
+								<span class="icon"><i class="flaticon-church"></i></span>
+								<div class="desc">
+									<span class="colorlib-counter js-counter" data-from="0" data-to="696969" data-speed="5000" data-refresh-interval="50"></span>
+									<span class="colorlib-counter-label">Delicious restaurants in your area</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4 text-center animate-box">
+							<div class="counter-entry">
+								<span class="icon"><i class="flaticon-dove"></i></span>
+								<div class="desc">
+									<span class="colorlib-counter js-counter" data-from="0" data-to="365" data-speed="5000" data-refresh-interval="50"></span>
+									<span class="colorlib-counter-label">Days a year of service</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<footer id="colorlib-footer">
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-3 colorlib-widget">
+						<h4>About JUBstEat</h4>
+						<p>JUBstEat is a collaborative effort to create a platform to help cardiologists around the world diagnose their patients through the use of artificial intelligence and machine learning.</p>
+						<p>
+							<ul class="colorlib-social-icons">
+								<li><a href="#"><i class="icon-twitter"></i></a></li>
+								<li><a href="#"><i class="icon-facebook"></i></a></li>
+								<li><a href="#"><i class="icon-linkedin"></i></a></li>
+								<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							</ul>
+						</p>
+					</div>
+
+					<div class="col-md-3 colorlib-widget">
+						<h4>Contact Info</h4>
+						<ul class="colorlib-footer-links">
+							<li>420 Restaurant Street,<br> Barcelona, Spain</li>
+							<li><a href="tel://645 82 11 79"><i class="icon-phone"></i> + 64 552 13 79</a></li>
+							<li><a href="mailto:info@yoursite.com"><i class="icon-envelope"></i> jubst_eat@gmail.com</a></li>
+							<li><a href="#"><i class="icon-location4"></i> JUBstEat.com</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="copy">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="/public/landing/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="/public/landing/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="/public/landing/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="/public/landing/js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="/public/landing/js/jquery.stellar.min.js"></script>
+	<!-- YTPlayer -->
+	<script src="/public/landing/js/jquery.mb.YTPlayer.min.js"></script>
+	<!-- Owl carousel -->
+	<script src="/public/landing/js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="/public/landing/js/jquery.magnific-popup.min.js"></script>
+	<script src="/public/landing/js/magnific-popup-options.js"></script>
+	<!-- Counters -->
+	<script src="/public/landing/js/jquery.countTo.js"></script>
+	<!-- Main -->
+	<script src="/public/landing/js/main.js"></script>
+
+	</body>
+</html>
